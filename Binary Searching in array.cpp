fbfc521b876c091ec4d777be53be3a702f1ef6d5
Muhuzair271 <muzair00303@gmail.com>
2024-04-28 21:43:34 +0500

    binary searching in array

--- Binary Searching in array.cpp
+++ Binary Searching in array.cpp
@@ -1,36 +1,358 @@
-//LINEAR SEARCHING
 
 #include<iostream>
 using namespace std;
-int  linearsearching(int array[], int size, int num){
-	for(int i=0; i<size; i++){
-		if(array[i]==num){
-			return num;
-		}	
+int  binarysearch(int array[], int size, int num){
+    //here function definition of the below function call
+	int low = 0;
+	int high = size - 1;
+	//cout<<"high:"<<high<<"low:"<<low;
+	while(low <= high){
+		
+		cout<<"low: "<<low<<"  "<< "high: "<<high;
+		cout<<endl;
+		int mid= (low+high) / 2; //find mid
+		cout<<endl;
+		cout<<"  mid:"<<mid<<"  "; 
+		cout<<endl;
+	if(array[mid]==num){
+			return mid;
+		}
+		if(array[mid]<num){
+			low=mid+1;
+		}else{
+		
+			high= mid-1;
+	}
+		
+	}
+	return -1;
+	
+}
+int main(){
+	int array[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+	int size= sizeof (array) / sizeof(array[0]);
+	cout<<"Array Element:-> ";
+    for(int i=0; i<size; i++){
+		cout<<array[i]<<"  ";
+	}
+	cout<<endl;
+	int num;
+	cout<<"Enter any above number:";
+	cin>>num;
+	int result= binarysearch(array, size, num); //function calling for above binarysearch
+	{
+		if(result==-1){       
+			cout<<"Number not found try again!"<<endl;
+		}else{
+			
+			cout<<"Number is found:"<<array[result];
+		}
+	}
+	return 0;
+}
+
+
+
+
+/*#include<iostream>
+using namespace std;
+int binarysearch (int array[], int size, int num){
+	int low = 0;
+	int high = size-1;
+	while(low <=  high) {
+		int mid = (low + high) / 2;
+		if(array[mid] == num){
+			return mid;
+		}
+		if(array[mid] < num){
+			low = mid + 1;
+		}
+		else{
+			high = mid + 1;
+		}
 	}
 	return -1;
 }
 int main(){
-	int array[]={8,9,2,4,7};
+	int array[] = {10,20,40,50,60};
+
+	int size = sizeof (array) / sizeof (array[0]);
+	int num = 40;
+	int result = binarysearch(array, size, num);//function calling
+	if(result == -1){
+		cout << "Element not found!";
+	}
+	else{	
+		cout << "Element Found:" << array[result];
+	}
+
+//	cout << int('a') << endl;
+//	cout << char(97) << endl;
 	
-	int size= sizeof (array) / sizeof (array[0]);
-	for(int i=0; i<size; i++){
-		cout<<array[i]<<" ";
+	return 0;		
+}*/
+
+
+
+
+
+
+
+//mnno
+/*
+#include<iostream>
+using namespace std;
+int  binarysearch(int array[], int size, int num){
+	int low=0;
+	int high=size-1;
+	//cout<<"low: "<<low<<" "<<"high: "<<high;
+while(low <= high){
+	cout<<"low: "<<low<<" "<<"high: "<<high<<"    ";
+	int mid = (low + high) / 2;
+	cout<<"mid:  "<<mid<<"  ";
+	if(array[mid] == num){
+		return mid;
+	}
+
+	
+}
+
+*/
+#include<iostream>
+using namespace std;
+int  binarysearch(int array[], int size, int num){
+    //here function definition of the below function call
+	int low = 0;
+	int high = size - 1;
+	//cout<<"high:"<<high<<"low:"<<low;
+	while(low <= high){
+		
+		cout<<"low: "<<low<<"  "<< "high: "<<high;
+		cout<<endl;
+		int mid= (low+high) / 2; //find mid
+		cout<<endl;
+		cout<<"  mid:"<<mid<<"  "; 
+		cout<<endl;
+	if(array[mid]==num){
+			return mid;
+		}
+		if(array[mid]<num){
+			low=mid+1;
+		}else{
+		
+			high= mid-1;
+	}
+		
+	}
+	return -1;
+	
+}
+int main(){
+	int array[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+	int size= sizeof (array) / sizeof(array[0]);
+	cout<<"Array Element:-> ";
+    for(int i=0; i<size; i++){
+		cout<<array[i]<<"  ";
 	}
 	cout<<endl;
 	int num;
-	cout<<"Enter any element:";
+	cout<<"Enter any above number:";
 	cin>>num;
-	int result=linearsearching(array, size, num);{
-	if(result==-1){
-		cout<<endl;
-		cout<<"Element not found!"<<endl;
-	}
-	else{
-		cout<<endl;
-		cout<<"Element found:"<<result<<endl;
-	}
-	
+	int result= binarysearch(array, size, num); //function calling for above binarysearch
+	{
+		if(result==-1){       
+			cout<<"Number not found try again!"<<endl;
+		}else{
+			
+			cout<<"Number is found:"<<array[result];
+		}
 	}
 	return 0;
 }
+
+
+
+
+/*#include<iostream>
+using namespace std;
+int binarysearch (int array[], int size, int num){
+	int low = 0;
+	int high = size-1;
+	while(low <=  high) {
+		int mid = (low + high) / 2;
+		if(array[mid] == num){
+			return mid;
+		}
+		if(array[mid] < num){
+			low = mid + 1;
+		}
+		else{
+			high = mid + 1;
+		}
+	}
+	return -1;
+}
+int main(){
+	int array[] = {10,20,40,50,60};
+
+	int size = sizeof (array) / sizeof (array[0]);
+	int num = 40;
+	int result = binarysearch(array, size, num);//function calling
+	if(result == -1){
+		cout << "Element not found!";
+	}
+	else{	
+		cout << "Element Found:" << array[result];
+	}
+
+//	cout << int('a') << endl;
+//	cout << char(97) << endl;
+	
+	return 0;		
+}*/
+
+
+
+
+
+
+
+//mnno
+/*
+#include<iostream>
+using namespace std;
+int  binarysearch(int array[], int size, int num){
+	int low=0;
+	int high=size-1;
+	//cout<<"low: "<<low<<" "<<"high: "<<high;
+while(low <= high){
+	cout<<"low: "<<low<<" "<<"high: "<<high<<"    ";
+	int mid = (low + high) / 2;
+	cout<<"mid:  "<<mid<<"  ";
+	if(array[mid] == num){
+		return mid;
+	}
+
+	
+}
+
+*/
+#include<iostream>
+using namespace std;
+int  binarysearch(int array[], int size, int num){
+    //here function definition of the below function call
+	int low = 0;
+	int high = size - 1;
+	//cout<<"high:"<<high<<"low:"<<low;
+	while(low <= high){
+		
+		cout<<"low: "<<low<<"  "<< "high: "<<high;
+		cout<<endl;
+		int mid= (low+high) / 2; //find mid
+		cout<<endl;
+		cout<<"  mid:"<<mid<<"  "; 
+		cout<<endl;
+	if(array[mid]==num){
+			return mid;
+		}
+		if(array[mid]<num){
+			low=mid+1;
+		}else{
+		
+			high= mid-1;
+	}
+		
+	}
+	return -1;
+	
+}
+int main(){
+	int array[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+	int size= sizeof (array) / sizeof(array[0]);
+	cout<<"Array Element:-> ";
+    for(int i=0; i<size; i++){
+		cout<<array[i]<<"  ";
+	}
+	cout<<endl;
+	int num;
+	cout<<"Enter any above number:";
+	cin>>num;
+	int result= binarysearch(array, size, num); //function calling for above binarysearch
+	{
+		if(result==-1){       
+			cout<<"Number not found try again!"<<endl;
+		}else{
+			
+			cout<<"Number is found:"<<array[result];
+		}
+	}
+	return 0;
+}
+
+
+
+
+/*#include<iostream>
+using namespace std;
+int binarysearch (int array[], int size, int num){
+	int low = 0;
+	int high = size-1;
+	while(low <=  high) {
+		int mid = (low + high) / 2;
+		if(array[mid] == num){
+			return mid;
+		}
+		if(array[mid] < num){
+			low = mid + 1;
+		}
+		else{
+			high = mid + 1;
+		}
+	}
+	return -1;
+}
+int main(){
+	int array[] = {10,20,40,50,60};
+
+	int size = sizeof (array) / sizeof (array[0]);
+	int num = 40;
+	int result = binarysearch(array, size, num);//function calling
+	if(result == -1){
+		cout << "Element not found!";
+	}
+	else{	
+		cout << "Element Found:" << array[result];
+	}
+
+//	cout << int('a') << endl;
+//	cout << char(97) << endl;
+	
+	return 0;		
+}*/
+
+
+
+
+
+
+
+//mnno
+/*
+#include<iostream>
+using namespace std;
+int  binarysearch(int array[], int size, int num){
+	int low=0;
+	int high=size-1;
+	//cout<<"low: "<<low<<" "<<"high: "<<high;
+while(low <= high){
+	cout<<"low: "<<low<<" "<<"high: "<<high<<"    ";
+	int mid = (low + high) / 2;
+	cout<<"mid:  "<<mid<<"  ";
+	if(array[mid] == num){
+		return mid;
+	}
+
+	
+}
+
+*/
